997a60773c0f010a6a3fc0f14e4a46b9511d84fe
sonalisenn <106011245+sonalisenn@users.noreply.github.com>
2022-05-26 01:01:00 +0530

    change login

--- settings.php
+++ settings.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/settings.css">
+    <link rel="stylesheet" href="css/settings.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -18,7 +18,7 @@
 
             <div class="change_img">
                 <div class="img">
-                    <img src="../images/4.jpg" alt="">
+                    <img src="images/4.jpg" alt="">
                 </div>
                 <div class="button">
                     <button class="btns1">Change Profile Picture</button>

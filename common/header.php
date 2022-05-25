83e9e1163398fb1d4a0b6078628596985325ef86
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-05-26 01:20:35 +0530

    include common file in index.php

--- common/header.php
+++ common/header.php
@@ -1,46 +1,9 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
-<html>
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="../css/header.css" />
-    <link href="../css/main.css" rel="stylesheet" />
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
-    <title></title>
-</head>
-
-<body>
 
     <header class="p">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <img class="logo" src="../images/_Bunz+logo+black+Copy.png" alt="" />
+                    <img class="logo" src="images/_Bunz+logo+black+Copy.png" alt="" />
                 </a>
 
                 <div class="forms col-12 col-lg-6 mb- mb-lg-0 me-lg-12 ">
@@ -90,7 +53,7 @@
         </div>
     </header>
 
-    <script src="../js/extention/choices.js"></script>
+    <script src="js/extention/choices.js"></script>
     <script>
         const choices = new Choices("[data-trigger]", {
             searchEnabled: false,
@@ -98,6 +61,4 @@
         });
     </script>
     <script src="https://kit.fontawesome.com/9d5313112d.js" crossorigin="anonymous"></script>
-</body>
 
-</html>

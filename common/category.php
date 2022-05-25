526f5e8a452dd85f8029061f8b537af8b13dcfbc
sonalisenn <106011245+sonalisenn@users.noreply.github.com>
2022-05-26 01:42:48 +0530

    add category

---
+++ common/category.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="" href="../css/category.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Category</title>
+</head>
+
+<body>
+    <div id="viewport">
+        <!-- Sidebar -->
+        <div id="sidebar">
+            <header>
+                <a href="#">Category</a>
+            </header>
+            <ul class="nav">
+                <li>
+                    <a href="#">
+                        Books
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Pens
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Papers
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                         Pencils
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                         Other
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- Content -->
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>

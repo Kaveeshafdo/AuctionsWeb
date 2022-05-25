6ced34b98f9c388cbb163f9b0dc77ca95ef1627b
sonalisenn <106011245+sonalisenn@users.noreply.github.com>
2022-05-26 00:12:15 +0530

    Update add_category.php

--- add_category.php
+++ add_category.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/item_for_trade.css">
+    <link rel="stylesheet" href="css/item_for_trade.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -16,7 +16,7 @@
     <div class="container">
         <div class="content">
             <div class="image">
-                <div class="img"> <img src="../images/add.svg" alt=""></div>
+                <div class="img"> <img src="images/add.svg" alt=""></div>
 
 
 
